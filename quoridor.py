8616a9f19ee6ab3a8c254048a43c88f3abdf5b26
alexi pittet <alexi.pittet.1@ulaval.ca>
2020-04-04 19:26:42 -0400

    méthode init de la classe

--- quoridor.py
+++ quoridor.py
@@ -1,8 +1,9 @@
+import random
+from itertools import product
 import networkx as nx
 
 
-# TODO: Définissez votre classe QuoridorError ici.
-
+class QuoridorError(Exception):
 
 class Quoridor:
     """Classe pour encapsuler le jeu Quoridor.
@@ -56,6 +57,52 @@ class Quoridor:
             self.gamestate = {'joueurs':
                               [self.joueur1, self.joueur2],
                               'murs': {'horizontaux': [], 'verticaux': []}}
+        
+        if isinstance(self.murs, dict):
+            self.gamestate['murs'] = self.murs
+
+            for i in murs['verticaux']:
+                if i[0] < 2 or i[1] > 8:
+                    raise QuoridorError("Position mur vertical invalide")
+                if i not in list(product(range(1, 10), repeat=2)):
+                    raise QuoridorError("Position mur vertical invalide")
+            
+            for i in murs['horizontaux']:
+                if i[0] > 8 or i[1] < 2:
+                    raise QuoridorError("Position mur horizontal invalide")
+                if i not in list(product(range(1, 10), repeat=2)):
+                    raise QuoridorError("Position mur horizontal invalide")
+
+            murs_dispos = joueurs[0].get('murs') + joueurs[1].get('murs')
+            murs_placés = len(murs['horizontaux']) + len(murs['verticaux'])
+            if murs_dispos + murs_placés != 20:
+                raise QuoridorError("Le total des murs placés et plaçables n'est pas égal à 20")
+
+        else:
+            if self.murs is not None:
+                raise QuoridorError("L'argument murs n'est pas un dictionnaire")
+
+        if isinstance(joueurs[0], dict):
+            for i in range(2):
+                if joueurs[i]['murs'] < 0 or joueurs[i]['murs'] > 10:
+                    raise QuoridorError("Nombre de murs qu'un joueur peut placer invalide")
+
+            for i in range(2):
+                if joueurs[i]['pos'] not in list(product(range(1, 10), repeat=2)):
+                    raise QuoridorError("Position du joueur invalide")
+
+            if murs is None:
+                if joueurs[0]['murs'] + joueurs[1]['murs'] != 20:
+                    raise QuoridorError("Le total des murs placés et plaçables n'est pas égal à 20")
+
+        if not hasattr(joueurs, '__iter__'):
+            raise QuoridorError("Argument joueurs n'est pas itérable")
+
+        if len(joueurs) > 2:
+            raise QuoridorError("Plus de deux joueurs")
+
+        self.position_interdites_horiz = []
+        self.position_interdites_verti = []
 
     def __str__(self):
         """Représentation en art ascii de l'état actuel de la partie.
